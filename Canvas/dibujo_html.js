494ede82ed65791365d2bbfe1ec844b05ca594fa
Pipecode65 <56938654+Pipecode65@users.noreply.github.com>
2019-11-01 16:17:56 -0500

    Update dibujo_html.js
    Mejora que estoy haciendo para generar mismo patreonde  dibujo pero en el sentido contrario

--- Canvas/dibujo_html.js
+++ Canvas/dibujo_html.js
@@ -1,1 +1,29 @@
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var yf, xi;
+while (l < lineas)
+{
+yi = 10 * l
+xf = 10 * (l + 1)
+dibujarLinea("#AFA", 0, yi, xf, 300)
+yf = 
+xi =
+dibujarLinea("#AFF",0, 0, 300, 300)
+l = l + 1
+}
 
+dibujarLinea("#AFA", 1, 1, 1, 299)
+dibujarLinea("#AFA", 1, 299, 299, 299)
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
