5a197feddbddd97edc1ffb6f6c11f5a4dca35a57
Pipecode65 <56938654+Pipecode65@users.noreply.github.com>
2019-11-05 16:47:11 -0500

    Update 11/05/2019

--- Canvas/dibujo_html.js
+++ Canvas/dibujo_html.js
@@ -4,19 +4,23 @@ var lineas = 30;
 var l = 0;
 var yi, xf;
 var yf, xi;
-while (l < lineas)
+var yf_2;
+var yi_2;
+var xf_2;
+for ( l = 0; l < lineas; l++)
 {
 yi = 10 * l
 xf = 10 * (l + 1)
+yf = 10 * (l + 1)
+xi = 10 * l
+yf_2 = 10 * (29 - l)
+yi_2 = 10 * ( 30 - l)
+xf_2 = 10 * (l + 1)
+dibujarLinea("#5A43A4", 0, yi_2, xf_2, 0)
+dibujarLinea("#FF9F33", xi, 300, 300, yf_2)
 dibujarLinea("#AFA", 0, yi, xf, 300)
-yf = 
-xi =
-dibujarLinea("#AFF",0, 0, 300, 300)
-l = l + 1
+dibujarLinea("#AFF", xi, 0, 300, yf)
 }
-
-dibujarLinea("#AFA", 1, 1, 1, 299)
-dibujarLinea("#AFA", 1, 299, 299, 299)
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
