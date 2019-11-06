fd0e909e5c5dfd8c380a02b39fe94aecd66e36d1
Pipecode65 <56938654+Pipecode65@users.noreply.github.com>
2019-11-06 13:24:11 -0500

    Update 11/06/2019 from work

--- Canvas/dibujo_html.js
+++ Canvas/dibujo_html.js
@@ -1,31 +1,10 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var yf, xi;
-var yf_2;
-var yi_2;
-var xf_2;
-var colorcito = "black";
-for ( l = 0; l < lineas; l++)
-{
-yi = 10 * l
-xf = 10 * (l + 1)
-yf = 10 * (l + 1)
-xi = 10 * l
-yf_2 = 10 * (29 - l)
-yi_2 = 10 * ( 30 - l)
-xf_2 = 10 * (l + 1)
-dibujarLinea("#5A43A4", 0, yi_2, xf_2, 0)
-dibujarLinea("#FF9F33", xi, 300, 300, yf_2)
-dibujarLinea("#AFA", 0, yi, xf, 300)
-dibujarLinea("#AFF", xi, 0, 300, yf)
-dibujarLinea(colorcito, 1, 1, 1, 299)
-dibujarLinea(colorcito, 1, 299, 299, 299)
-dibujarLinea(colorcito, 299, 299, 299, 1)
-dibujarLinea(colorcito, 299, 1, 1, 1)
-}
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -35,4 +14,36 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
   lienzo.lineTo(xfinal, yfinal);
   lienzo.stroke();
   lienzo.closePath();
+}
+
+function dibujoPorClick()
+{
+  var xxx = parseInt(texto.value);
+  var lineas = xxx;
+  var espacio = ancho / lineas;
+  var l = 0;
+  var yi, xf;
+  var yf, xi;
+  var yf_2;
+  var yi_2;
+  var xf_2;
+  var colorcito = "black";
+  for ( l = 0; l < lineas; l++)
+  {
+  yi = espacio * l
+  xf = espacio * (l + 1)
+  yf = espacio * (l + 1)
+  xi = espacio * l
+  yf_2 = espacio * (lineas - l)
+  yi_2 = espacio * ( lineas - l)
+  xf_2 = espacio * (l + 1)
+  dibujarLinea("#5A43A4", 0, yi_2, xf_2, 0)
+  dibujarLinea("#FF9F33", xi, 300, 300, yf_2)
+  dibujarLinea("#AFA", 0, yi, xf, 300)
+  dibujarLinea("#AFF", xi, 0, 300, yf)
+  dibujarLinea(colorcito, 1, 1, 1, 299)
+  dibujarLinea(colorcito, 1, 299, 299, 299)
+  dibujarLinea(colorcito, 299, 299, 299, 1)
+  dibujarLinea(colorcito, 299, 1, 1, 1)
+  }
 }
